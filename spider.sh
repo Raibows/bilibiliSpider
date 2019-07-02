ebbee42efeb6ecd43b746f58c3a6d8353cfd1ae9
raibows <raibows@hotmail.com>
2019-07-02 13:07:39 +0800

    get ready for Cloud

--- spider.sh
+++ spider.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/admin/projects/bilibilivideohot
 source /home/admin/projects/bilibilivideohot/venv/bin/activate
-python3 /home/admin/projects/bilibilivideohot/spider_ranklist.py
+python3 /home/admin/projects/bilibilivideohot/main.py
